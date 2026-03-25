e26807457d2c04d0f6c34a30ba4aeba6d0a07d03
Evan <75786458+EmrysChoo@users.noreply.github.com>
2026-03-25 13:38:23 +0800

    Update generateFwd.js

--- Forward/generateFwd.js
+++ Forward/generateFwd.js
@@ -11,15 +11,28 @@ console.log("找到的文件:", files);
 function extractMeta(filePath) {
   const content = fs.readFileSync(filePath, "utf-8");
 
-  // 只匹配 WidgetMetadata 对象里的顶层字段，不解析 modules
+  // 只解析 WidgetMetadata 对象里的顶层字段，不要 modules
   const meta = {};
-  meta.id = (content.match(/WidgetMetadata\s*=\s*{[^}]*id:\s*["'](.+?)["']/) || [])[1] || path.basename(filePath, ".js");
-  meta.title = (content.match(/WidgetMetadata\s*=\s*{[^}]*title:\s*["'](.+?)["']/) || [])[1] || meta.id;
-  meta.description = (content.match(/WidgetMetadata\s*=\s*{[^}]*description:\s*["'](.+?)["']/) || [])[1] || "";
-  meta.version = (content.match(/WidgetMetadata\s*=\s*{[^}]*version:\s*["'](.+?)["']/) || [])[1] || "0.0.1";
-  meta.requiredVersion = (content.match(/WidgetMetadata\s*=\s*{[^}]*requiredVersion:\s*["'](.+?)["']/) || [])[1] || "0.0.1";
-  meta.author = (content.match(/WidgetMetadata\s*=\s*{[^}]*author:\s*["'](.+?)["']/) || [])[1] || "unknown";
-  meta.site = (content.match(/WidgetMetadata\s*=\s*{[^}]*site:\s*["'](.+?)["']/) || [])[1] || "";
+  const widgetBlock = content.match(/WidgetMetadata\s*=\s*{([\s\S]*?)}/);
+  if (widgetBlock) {
+    const block = widgetBlock[1];
+    meta.id = (block.match(/id:\s*["'](.+?)["']/) || [])[1] || path.basename(filePath, ".js");
+    meta.title = (block.match(/title:\s*["'](.+?)["']/) || [])[1] || meta.id;
+    meta.description = (block.match(/description:\s*["'](.+?)["']/) || [])[1] || "";
+    meta.version = (block.match(/version:\s*["'](.+?)["']/) || [])[1] || "0.0.1";
+    meta.requiredVersion = (block.match(/requiredVersion:\s*["'](.+?)["']/) || [])[1] || "0.0.1";
+    meta.author = (block.match(/author:\s*["'](.+?)["']/) || [])[1] || "unknown";
+    meta.site = (block.match(/site:\s*["'](.+?)["']/) || [])[1] || "";
+  } else {
+    // 没有 WidgetMetadata 就用默认值
+    meta.id = path.basename(filePath, ".js");
+    meta.title = meta.id;
+    meta.description = "";
+    meta.version = "0.0.1";
+    meta.requiredVersion = "0.0.1";
+    meta.author = "unknown";
+    meta.site = "";
+  }
 
   return meta;
 }
